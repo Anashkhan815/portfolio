aeefd6797597a362092a4664c9418ef81db8147b
Anashkhan815 <112409806+Anashkhan815@users.noreply.github.com>
2023-07-04 11:55:27 -0700

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,6 @@
+(function(){
+let screen = document.querySelector('.screen');
+let button = document.querySelectorAll('.btn');
+let screen  = document.querySelector('.screen');
+let screen = document.querySelector('.screen');
+})();
